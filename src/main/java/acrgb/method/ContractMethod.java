2b92f81af8b90c54a2d9a12abb18b7ac923c2255
MinoSun <123798384+jclibredo@users.noreply.github.com>
2024-07-03 14:22:38 +0800

    g

--- src/main/java/acrgb/method/ContractMethod.java
+++ src/main/java/acrgb/method/ContractMethod.java
@@ -741,9 +741,7 @@ public class ContractMethod {
                         for (int y = 0; y < HCPNList.size(); y++) {
                             //GET ALL FACILITY UNDER HCPN
                             ACRGBWSResult totalResult = methods.GETSUMMARY(dataSource, HCPNList.get(y).trim());
-                            // System.out.println("HCF Code : " + HCPNList.get(y).trim());
                             if (totalResult.isSuccess()) {
-                                // System.out.println("Contract List : " + totalResult.getResult());
                                 Total getResult = utility.ObjectMapper().readValue(totalResult.getResult(), Total.class);
                                 tranches += Integer.parseInt(getResult.getCcount());
                                 tracnheamountreleased += Double.parseDouble(getResult.getCtotal());
@@ -753,23 +751,22 @@ public class ContractMethod {
                             if (GetHCPNContract.isSuccess()) {
                                 Contract MapHCPNContract = utility.ObjectMapper().readValue(GetHCPNContract.getResult(), Contract.class);
                                 ContractDate condate = utility.ObjectMapper().readValue(MapHCPNContract.getContractdate(), ContractDate.class);
-//                                  System.out.println(MapHCPNContract);
-//                                    System.out.println(condate);
                                 ACRGBWSResult GetHCIList = methods.GETROLEMULITPLE(dataSource, HCPNList.get(y).trim(), tags.trim());
                                 if (GetHCIList.isSuccess()) {
-//                                    List<String> HCIList = Arrays.asList(GetHCIList.getResult().split(","));
-//                                    for (int x = 0; x < HCIList.size(); x++) {
-//                                        ACRGBWSResult sumresult = fm.GETNCLAIMS(dataSource, HCIList.get(x).trim(), "G", condate.getDatefrom(), condate.getDateto(), "CURRENTSTATUS");
-//                                        if (sumresult.isSuccess()) {
-//                                            List<NclaimsData> nclaimsdata = Arrays.asList(utility.ObjectMapper().readValue(sumresult.getResult(), NclaimsData[].class));
-//                                            System.out.println("Naclaims Data List: " + utility.ObjectMapper().writeValueAsString(nclaimsdata));
-//                                            for (int i = 0; i < nclaimsdata.size(); i++) {
-//                                                numberofclaims += Integer.parseInt(nclaimsdata.get(i).getTotalclaims());
-//                                                totalclaimsamount += Double.parseDouble(nclaimsdata.get(i).getClaimamount());
-//                                            }
-//                                        }
-//                                    }
+                                    List<String> HCIList = Arrays.asList(GetHCIList.getResult().split(","));
+                                    for (int x = 0; x < HCIList.size(); x++) {
+                                        ACRGBWSResult sumresult = fm.GETNCLAIMS(dataSource, HCIList.get(x).trim(), "G", condate.getDatefrom(), condate.getDateto(), "CURRENTSTATUS");
+                                        if (sumresult.isSuccess()) {
+                                            List<NclaimsData> nclaimsdata = Arrays.asList(utility.ObjectMapper().readValue(sumresult.getResult(), NclaimsData[].class));
+                                            System.out.println("Naclaims Data List: " + utility.ObjectMapper().writeValueAsString(nclaimsdata));
+                                            for (int i = 0; i < nclaimsdata.size(); i++) {
+                                                numberofclaims += Integer.parseInt(nclaimsdata.get(i).getTotalclaims());
+                                                totalclaimsamount += Double.parseDouble(nclaimsdata.get(i).getClaimamount());
+                                            }
+                                        }
+                                    }
                                 }
+                                
                             }
                         }
                     }
