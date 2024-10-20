26cb5b882b22ea6e6a4b035bbeff478697a11c9c
MinoSun <123798384+jclibredo@users.noreply.github.com>
2024-10-20 22:55:38 +0800

    g

--- src/main/java/acrgb/method/FacilityTagging.java
+++ src/main/java/acrgb/method/FacilityTagging.java
@@ -8,9 +8,11 @@ package acrgb.method;
 import acrgb.structure.ACRGBWSResult;
 import acrgb.structure.Tagging;
 import acrgb.utility.Utility;
+import java.io.IOException;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.Date;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -38,12 +40,11 @@ public class FacilityTagging {
         result.setMessage("");
         result.setResult("");
         result.setSuccess(false);
-//        java.util.Date d1 = new java.util.Date();
         ArrayList<String> errorList = new ArrayList<>();
         try (Connection connection = datasource.getConnection()) {
             List<String> serviceList = Arrays.asList(servicetype.split(","));
             for (int i = 0; i < serviceList.size(); i++) {
-                CallableStatement getinsertresult = connection.prepareCall("call ACR_GB.ACRGBFACILITYTAGGING(:Message,:Code,:upmcc_no,:uservicetype,:udatestart,:udateended,:ucreator,:udatecreated,:udateissue,:udateeffective)");
+                CallableStatement getinsertresult = connection.prepareCall("call ACR_GB.ACRGBACCREPACKAGE.ACRGBFACILITYTAGGING(:Message,:Code,:upmcc_no,:uservicetype,:udatestart,:udateended,:ucreator,:udatecreated,:udateissue,:udateeffective)");
                 getinsertresult.registerOutParameter("Message", OracleTypes.VARCHAR);
                 getinsertresult.registerOutParameter("Code", OracleTypes.INTEGER);
                 getinsertresult.setString("upmcc_no", tagging.getHcino());
@@ -57,6 +58,18 @@ public class FacilityTagging {
                 getinsertresult.execute();
                 if (!getinsertresult.getString("Message").equals("SUCC")) {
                     errorList.add(getinsertresult.getString("Message"));
+                } else {
+                    if (this.CHECKIFEXIST(datasource, tagging.getHcino(), serviceList.get(i).trim().toUpperCase()).isSuccess()) {
+                        ACRGBWSResult update = this.ACRGBHCITAGUPDATE(datasource, tagging, serviceList.get(i).trim().toUpperCase());
+                        if (!update.isSuccess()) {
+                            errorList.add(getinsertresult.getString("Message"));
+                        }
+                    } else {
+                        ACRGBWSResult addnew = this.ACRGBHCITAGNEW(datasource, tagging, serviceList.get(i).trim().toUpperCase());
+                        if (!addnew.isSuccess()) {
+                            errorList.add(getinsertresult.getString("Message"));
+                        }
+                    }
                 }
             }
             if (errorList.size() > 0) {
@@ -72,34 +85,99 @@ public class FacilityTagging {
         return result;
     }
 
-//    public ACRGBWSResult TaggApexFacility(final DataSource datasource, Tagging tagging) {
-//        ACRGBWSResult result = utility.ACRGBWSResult();
-//        result.setMessage("");
-//        result.setResult("");
-//        result.setSuccess(false);
-//        try (Connection connection = datasource.getConnection()) {
-//            CallableStatement getinsertresult = connection.prepareCall("call ACR_GB.ACRGBFACILITYTAGGING(:Message,:Code,:upmcc_no,:uservicetype,:udatestart,:udateended,:ucreator,:udatecreated,:udateissue,:udateeffective)");
-//            getinsertresult.registerOutParameter("Message", OracleTypes.VARCHAR);
-//            getinsertresult.registerOutParameter("Code", OracleTypes.INTEGER);
-//            getinsertresult.setString("upmcc_no", tagging.getHcino());
-//            getinsertresult.setString("uservicetype", tagging.getAncservicetype());
-//            getinsertresult.setDate("udatestart", (Date) new Date(utility.StringToDate(tagging.getStartdate()).getTime()));//tranch.getDatecreated());
-//            getinsertresult.setDate("udateended", (Date) new Date(utility.StringToDate(tagging.getExpireddate()).getTime()));//tranch.getDatecreated());
-//            getinsertresult.setString("ucreator", tagging.getUsername());
-//            getinsertresult.setDate("udatecreated", (Date) new Date(utility.StringToDate(tagging.getEntrydate()).getTime()));//tranch.getDatecreated());
-//            getinsertresult.setDate("udateissue", (Date) new Date(utility.StringToDate(tagging.getIssuedate()).getTime()));//tranch.getDatecreated());
-//            getinsertresult.setDate("udateeffective", (Date) new Date(utility.StringToDate(tagging.getEffdate()).getTime()));//tranch.getDatecreated());
-//            getinsertresult.execute();
-//            if (getinsertresult.getString("Message").equals("SUCC")) {
-//                result.setSuccess(true);
-//                result.setMessage(getinsertresult.getString("Message"));
-//            } else {
-//                result.setMessage(getinsertresult.getString("Message"));
-//            }
-//        } catch (SQLException ex) {
-//            result.setMessage(ex.toString());
-//            Logger.getLogger(FacilityTagging.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//        return result;
-//    }
+    public ACRGBWSResult CHECKIFEXIST(
+            final DataSource dataSource,
+            final String pmccno,
+            final String servicetype) {
+        ACRGBWSResult result = utility.ACRGBWSResult();
+        result.setMessage("");
+        result.setResult("");
+        result.setSuccess(false);
+        try (Connection connection = dataSource.getConnection()) {
+            CallableStatement statement = connection.prepareCall("begin :v_result := ACR_GB.ACRGBACCREPACKAGE.CHECKIFEXIST(:upmcc_no,:uservicetype); end;");
+            statement.registerOutParameter("v_result", OracleTypes.CURSOR);
+            statement.setString("upmcc_no", pmccno);
+            statement.setString("uservicetype", servicetype);
+            statement.execute();
+            ResultSet resultset = (ResultSet) statement.getObject("v_result");
+            if (resultset.next()) {
+                Tagging tagging = new Tagging();
+                tagging.setAncservicetype(resultset.getString("ANC_SERVICE_TYPE"));
+                tagging.setHcino(resultset.getString("HCI_NO"));
+                result.setSuccess(true);
+                result.setResult(utility.ObjectMapper().writeValueAsString(tagging));
+            } else {
+                result.setMessage("N/A");
+            }
+        } catch (SQLException | IOException ex) {
+            result.setMessage(ex.toString());
+            Logger.getLogger(FetchMethods.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
+
+    public ACRGBWSResult ACRGBHCITAGUPDATE(final DataSource datasource,
+            final Tagging tagging,
+            final String serviceType) {
+        ACRGBWSResult result = utility.ACRGBWSResult();
+        result.setMessage("");
+        result.setResult("");
+        result.setSuccess(false);
+        try (Connection connection = datasource.getConnection()) {
+            CallableStatement getinsertresult = connection.prepareCall("call ACR_GB.ACRGBACCREPACKAGE.ACRGBHCITAGUPDATE(:Message,:Code,:upmcc_no,:uservicetype,:udatestart,:udateended,:udatecreated,:udateissue,:udateeffective)");
+            getinsertresult.registerOutParameter("Message", OracleTypes.VARCHAR);
+            getinsertresult.registerOutParameter("Code", OracleTypes.INTEGER);
+            getinsertresult.setString("upmcc_no", tagging.getHcino());
+            getinsertresult.setString("uservicetype", serviceType.trim().toUpperCase());
+            getinsertresult.setDate("udatestart", (Date) new Date(utility.StringToDate(tagging.getStartdate()).getTime()));//tranch.getDatecreated());
+            getinsertresult.setDate("udateended", (Date) new Date(utility.StringToDate(tagging.getExpireddate()).getTime()));//tranch.getDatecreated());
+            getinsertresult.setTimestamp("udatecreated", new java.sql.Timestamp(utility.GetCurrentDate().getTime()));//tranch.getDatecreated());
+            getinsertresult.setDate("udateissue", (Date) new Date(utility.StringToDate(tagging.getIssuedate()).getTime()));//tranch.getDatecreated());
+            getinsertresult.setDate("udateeffective", (Date) new Date(utility.StringToDate(tagging.getEffdate()).getTime()));//tranch.getDatecreated());
+            getinsertresult.execute();
+            if (!getinsertresult.getString("Message").equals("SUCC")) {
+                result.setMessage(getinsertresult.getString("Message"));
+            } else {
+                result.setMessage(getinsertresult.getString("Message"));
+                result.setSuccess(true);
+            }
+        } catch (SQLException ex) {
+            result.setMessage(ex.toString());
+            Logger.getLogger(FacilityTagging.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
+
+    public ACRGBWSResult ACRGBHCITAGNEW(final DataSource datasource,
+            final Tagging tagging,
+            final String servicetype) {
+        ACRGBWSResult result = utility.ACRGBWSResult();
+        result.setMessage("");
+        result.setResult("");
+        result.setSuccess(false);
+        try (Connection connection = datasource.getConnection()) {
+            CallableStatement getinsertresult = connection.prepareCall("call ACR_GB.ACRGBACCREPACKAGE.ACRGBHCITAGNEW(:Message,:Code,:upmcc_no,:uservicetype,:udatestart,:udateended,:ucreator,:udatecreated,:udateissue,:udateeffective)");
+            getinsertresult.registerOutParameter("Message", OracleTypes.VARCHAR);
+            getinsertresult.registerOutParameter("Code", OracleTypes.INTEGER);
+            getinsertresult.setString("upmcc_no", tagging.getHcino());
+            getinsertresult.setString("uservicetype", servicetype.trim().toUpperCase());
+            getinsertresult.setDate("udatestart", (Date) new Date(utility.StringToDate(tagging.getStartdate()).getTime()));//tranch.getDatecreated());
+            getinsertresult.setDate("udateended", (Date) new Date(utility.StringToDate(tagging.getExpireddate()).getTime()));//tranch.getDatecreated());
+            getinsertresult.setString("ucreator", tagging.getUsername());
+            getinsertresult.setTimestamp("udatecreated", new java.sql.Timestamp(utility.GetCurrentDate().getTime()));//tranch.getDatecreated());
+            getinsertresult.setDate("udateissue", (Date) new Date(utility.StringToDate(tagging.getIssuedate()).getTime()));//tranch.getDatecreated());
+            getinsertresult.setDate("udateeffective", (Date) new Date(utility.StringToDate(tagging.getEffdate()).getTime()));//tranch.getDatecreated());
+            getinsertresult.execute();
+            if (!getinsertresult.getString("Message").equals("SUCC")) {
+                result.setMessage(getinsertresult.getString("Message"));
+            } else {
+                result.setMessage(getinsertresult.getString("Message"));
+                result.setSuccess(true);
+            }
+        } catch (SQLException ex) {
+            result.setMessage(ex.toString());
+            Logger.getLogger(FacilityTagging.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
 }
